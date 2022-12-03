e242cad76b47ac66564bf3199499eb5e5fc0e732
Gabriel <gabrielwambugu13@yahoo.co.uk>
2022-12-03 17:09:06 +0300

    Arithmetic lab done

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+let num1 = 31, num2 = 2;
+let multiply = num1* num2;
+
+let random = Math.round(Math.random()* 10);
+
+let num3 = 39, num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(34, 22, 45, 67, 55, 2345, 5443);
+
+console.log(multiply)
+
+console.log(random)
+
+console.log(mod)
+
+console.log(max)
